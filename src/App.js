4af2a0009662e39068f302d3964ffd6214604a38
Pythony <contact@anthony-khelil.com>
2021-03-19 17:35:59 +0100

    Signature Pad

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import React from "react"
 
 import ReactCode from "./components/reactCode"
+import ReactPad from "./components/reactPad"
+
+import "./App.css"
 
 export default function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <ReactCode id="AnthonyParis"/>
+                <ReactPad/>
             </header>
         </div>
     )
---
+++ src/components/reactPad.js
@@ -0,0 +1,14 @@
+
+import React from "react"
+
+const SignaturePad = require("react-signature-pad")
+
+const ReactPad = props => {
+    return (
+        <div>
+            <SignaturePad/>
+        </div>
+    )
+}
+
+export default ReactPad
