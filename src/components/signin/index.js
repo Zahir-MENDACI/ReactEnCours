3f436c2a10a8af1074d894c5cfdbb881711b778e
christmiller <christmiller@gmail.com>
2021-03-25 14:04:43 +0100

    second commit

--- src/components/signin/index.js
+++ src/components/signin/index.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import styled from 'styled-components';
-
+import axios from 'axios';
+import{useHistory, userHistory} from 'react-router-dom'
+const Signin  = props => {
+    
+    const[username, setUsername] = useState('')
+    const[ password, setPassword] = useState('')
+    const history = useHistory()
+    console.log('history',history)
 const submit = e =>{
     e.preventDefault()
-    console.log(e.target)
+    console.log(username)
+    console.log(password)
 }
-const Signin  = () => {
+axios({
+    method: 'POST',
+    url:'https://easy-login-api.herokuapp.com/users/login',
+    data: {
+        username:username,
+        password:password
+    }
+})   
+    .then(res =>{
+        console.log(res.headers['x-access-token'])
+        localStorage.setItem('token',res.headers['x-access-token'])
+        history.push('/home/20')
+    })
+    .catch(err =>{
+        console.log(err)
+    })
+
     return (
         <StyledForm onSubmit={submit}>
             <StyledSpan>Signin</StyledSpan>
-            <SigninInput placeholder="Username" type ='text'></SigninInput>
-            <SigninInput placeholder="Password" type ='password'></SigninInput>
+            <SigninInput placeholder="username" onChange={e => setUsername(e.target.value)}type ='text'></SigninInput>
+            <SigninInput placeholder="password" onChange={e => setPassword(e.target.value)} type ='password'></SigninInput>
             <SigninInput type='Submit'></SigninInput>
             
         </StyledForm>
