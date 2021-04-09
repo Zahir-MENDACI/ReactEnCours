76a7b52fbc6a39c96aac13dec6003821cf883efd
Zahir MENDACI <75799200+Zahir-MENDACI@users.noreply.github.com>
2021-04-10 00:03:27 +0200

    Update Code.js

--- src/components/Code.js
+++ src/components/Code.js
@@ -4,7 +4,7 @@ import QRCode from 'qrcode.react'
  const Code = () => {
      return (
          <div>
-             <QRCode value={ "https://signature-app-react.herokuapp.com//signature/"} size={300}/>
+             <QRCode value={ "https://signature-app-react.herokuapp.com/signature/"} size={300}/>
          </div>
      )
  }
